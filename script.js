fc3a43f875e39e98cb2bac74d44ee850d19e0506
Cleython-Enginner <cleythonsantos@gmail.com>
2021-08-06 21:20:20 -0300

    Muda link Iframe

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 let janela = document.querySelector('.janela');
-let menu = document.querySelector('.menu')
-
+let menu = document.querySelector('.menu');
+let iframe =document.querySelector('.iframe');
 
 function abrirMenu(){
   janela.classList.toggle('abrirMenu');
   menu.classList.toggle('abrirMenu');
 }
 
+function abrirIframe(){
+  janela.classList.toggle('abrirIframe');
+  menu.classList.toggle('abrirIframe');
+}
 menu.onclick = abrirMenu;
+return abrirIframe;
+
+
+
+
+
